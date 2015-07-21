0e38fae4c28dee30833dfe5aca19826cfa81ab72
William Billingsley <wbilling@une.edu.au>
2015-07-22 09:11:11 +1000

    Added skeleton of the captcha app for the tutorial

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,16 +6,12 @@ import play.mvc.Result;
 
 public class Application extends Controller {
 
-    public static Result encrypt(String pw) {
-        return ok(views.html.application.encrypted.render(BCryptExample.encrypt(pw)));
-    }
-
-    public static Result matches(String pw) {
-        return ok(views.html.application.matches.render(BCryptExample.matchesLastEncrypted(pw)));
-    }
-
     public static Result index() {
-        return ok(views.html.application.index.render());
+        return internalServerError("Not implemented yet...");
+    }
+
+    public static Result matches() {
+        return internalServerError("Not implemented yet...");
     }
 
 }
--- app/model/Captcha.java
+++ app/model/Captcha.java
@@ -6,7 +6,11 @@ public class Captcha {
      * URLs of photos the user should select
      */
     public static String[] yesPhotos = {
-
+            "http://assets.dogtime.com/breed/profile/image/4d3771f10106195669001f8c/beagle.jpg",
+            "https://gdkennel.files.wordpress.com/2013/09/010.jpg",
+            "http://www.perros2.com/wp-content/uploads/2012/06/ddd.jpg",
+            "http://dogobedienceadvice.com/images/beagle_training.jpg",
+            "http://www.swish-swash.co.uk/images/beagle/beagles.jpg"
     };
 
 
@@ -14,7 +18,11 @@ public class Captcha {
      * URLs of photos the user should not select
      */
     public static String[] noPhotos = {
-
+            "http://www.pets4homes.co.uk/images/articles/1265/large/ten-reasons-why-dogs-make-better-pets-than-cats-52bc3172b4816.jpg",
+            "http://www.adweek.com/files/imagecache/node-blog/sa_article/dog_image_0.jpg",
+            "http://7-themes.com/data_images/out/36/6891162-dogs.jpg",
+            "http://www.boxer-dog.org/sites/default/files/imagecache/500_width/ruby2.jpg",
+            "http://www.imagepuppy.com/resized/762ce6cb8241c08d73ec9304b42f6d5f.jpg"
     };
 
     /**
